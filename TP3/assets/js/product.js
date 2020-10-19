6ae9097689db1217cbb4f4cb1e41e4606b3567a5
Annie <annie.rochette@hec.ca>
2020-10-19 14:07:52 -0400

    Page des produits fonctionnelles

--- TP3/assets/js/product.js
+++ TP3/assets/js/product.js
@@ -1,5 +1,27 @@
+
 //TODO: Récupérer le paramètre dans l'URL
 //TODO: Fetch + afficher les informations du produit
+
+var products;
+
+var req = new XMLHttpRequest();
+var url = "data/products.json";
+
+//Get la liste des produits
+req.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+        products = JSON.parse(this.response);
+        console.log(products);
+    }
+}
+req.open("GET", url, true);
+req.send();
+
+$(function() {
+  console.log( 'ready!' );
+});
+
+
 
 //TODO: Page d'erreur si ID invalide
 
--- TP3/assets/js/products.js
+++ TP3/assets/js/products.js
@@ -1,10 +1,10 @@
 
-//TODO: Rendre fonctionnel le groupe de bouton "product-categories" (classe selected à la sélection)
-//TODO: Rendre fonctionnel le groupe de bouton "product-criteria" (classe selected à la sélection)
-
 var criteria = "price";
 var sortOrder = "asc"
-var categorie = "";
+var category = "all";
+
+var products;
+var sortedProducts;
 
 var req = new XMLHttpRequest();
 var url = "data/products.json";
@@ -12,32 +12,76 @@ var url = "data/products.json";
 //Get la liste des produits
 req.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
-        var products = JSON.parse(this.response);
+        products = JSON.parse(this.response);
 
-        //Trier la liste
-        products.sort(compareValues(criteria, sortOrder));
-
-        createProducts(products);
-        console.log(products);
+        createProducts();
     }
 }
 req.open("GET", url, true);
 req.send();
 
+//Groupe de boutons critères/catégories
+$(function() {
+
+  $('#product-categories').children('button').each(function () {
+    $(this).on("click", function() {
+      $(this).siblings('.selected').removeClass('selected');
+      $(this).addClass('selected');
+      var newCat = $(this).attr('name');
+      category = newCat;
+
+      createProducts();
+    });
+  });
+
+  $('#product-criteria').children('button').each(function () {
+    $(this).on("click", function() {
+      $(this).siblings('.selected').removeClass('selected');
+      $(this).addClass('selected');
+      var newCrit = $(this).attr('name').split("-");
+      criteria = newCrit[0];
+      sortOrder = newCrit[1];
+
+      createProducts();
+    });
+  });
+
+});
+
+//Filtre les produits par catégorie
+function sortCategories() {
+  var newArr = [];
+  products.forEach(prod => {
+    if(prod.category == category){
+      newArr.push(prod);
+    }
+  });
+
+  return newArr;
+}
+
 //Crée les fiches de produits
-function createProducts(array) {
+function createProducts() {
     var out = "";
     var i;
+    sortedProducts = products;
 
-    for(i = 0; i < array.length; i++){
-        //var productURL = "./product.html?id=#" + array[i].id; //Formatage du lien pour affichage produit
-        out += '<div class="product"> <a href="./product.html" title="En savoir plus..."> <h2>' + 
-        array[i].name + '</h2> <img alt="' + array[i].name + '" src="./assets/img/' + 
-        array[i].image + '"> <p class="price"><small>Prix</small>  ' + array[i].price +'&thinsp;$</p> </a></div>'
+    //Filtrer les catégories
+    if(category != "all"){
+      sortedProducts = sortCategories();
+    }
+
+    //Trier selon les critères
+    sortedProducts = sortedProducts.sort(compareValues(criteria, sortOrder));
+
+    for(i = 0; i < sortedProducts.length; i++){
+        out += '<div class="product"> <a href="./product.html?id=#'+ sortedProducts[i].id + '" title="En savoir plus..."> <h2>' + 
+        sortedProducts[i].name + '</h2> <img alt="' + sortedProducts[i].name + '" src="./assets/img/' + 
+        sortedProducts[i].image + '"> <p class="price"><small>Prix</small>  ' + sortedProducts[i].price +'&thinsp;$</p> </a></div>'
     }
 
     //Affiche le bon nombre de produits
-    document.getElementById("products-count").innerText = array.length + " produits"
+    document.getElementById("products-count").innerText = sortedProducts.length + " produits"
     //Affiche les fiches
     document.getElementById("products-list").innerHTML = out;
 }
