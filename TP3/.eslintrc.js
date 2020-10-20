88ae59f1cf1971345d14de02ec513140316e1962
Annie <annie.rochette@hec.ca>
2020-10-20 13:58:00 -0400

    Ajout au Lint pour variables d'environnement

--- TP3/.eslintrc.js
+++ TP3/.eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
     "env": {
         "node": true,
         "es6": true,
+        "browser": true,
+        "jquery": true,
+        "commonjs": true,
     },
     "extends": "eslint:recommended",
     "globals": {
