452063cfe5590ee1a35b64ce7663184b47beea66
maxime.bernier <maxime.bernier@polymtl.ca>
2020-11-03 16:38:23 -0500

    tp4: Ajouter les routes et les templates pour head, header, footer

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render("index", {title: "Accueil", message: "Ça semble fonctionner!"});
+router.get(["/", "/accueil"], (req, res) => {
+    res.render("../views/pages/index", {title: "Accueil", message: "Ça semble fonctionner!"});
+});
+
+router.get("/produits", (req, res) => {
+    res.render("../views/pages/products", {title: "Produits", message: "Ça semble fonctionner!"});
+});
+
+router.get("/produits/:id", (req, res) => {
+    res.render("../views/pages/product", {title: "Produit", message: "Ça semble fonctionner!"});
+});
+
+router.get("/contact", (req, res) => {
+    res.render("../views/pages/contact", {title: "Contact", message: "Ça semble fonctionner!"});
+});
+
+router.get("/panier", (req, res) => {
+    res.render("../views/pages/shopping-cart", {title: "Panier", message: "Ça semble fonctionner!"});
+});
+
+router.get("/commande", (req, res) => {
+    res.render("../views/pages/order", {title: "Commande", message: "Ça semble fonctionner!"});
+});
+
+router.get("/confirmation", (req, res) => {
+    res.render("../views/pages/confirmation", {title: "Confirmation", message: "Ça semble fonctionner!"});
 });
 
 module.exports = router;
