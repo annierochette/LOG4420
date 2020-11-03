cf5d7ab79529bcd0684494d40940c6cc721bdd68
maxime.bernier <maxime.bernier@polymtl.ca>
2020-11-03 17:29:13 -0500

    tp4: Le compte du panier vient de l'objet dans index.js

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -2,31 +2,31 @@ const express = require("express");
 const router = express.Router();
 
 router.get(["/", "/accueil"], (req, res) => {
-    res.render("../views/pages/index", {title: "Accueil", message: "Ça semble fonctionner!"});
+    res.render("../views/pages/index", {title: "Accueil", shoppingCartCount: 5 });
 });
 
 router.get("/produits", (req, res) => {
-    res.render("../views/pages/products", {title: "Produits", message: "Ça semble fonctionner!"});
+    res.render("../views/pages/products", {title: "Produits", shoppingCartCount: 5 });
 });
 
 router.get("/produits/:id", (req, res) => {
-    res.render("../views/pages/product", {title: "Produit", message: "Ça semble fonctionner!"});
+    res.render("../views/pages/product", {title: "Produit", shoppingCartCount: 5 });
 });
 
 router.get("/contact", (req, res) => {
-    res.render("../views/pages/contact", {title: "Contact", message: "Ça semble fonctionner!"});
+    res.render("../views/pages/contact", {title: "Contact", shoppingCartCount: 5 });
 });
 
 router.get("/panier", (req, res) => {
-    res.render("../views/pages/shopping-cart", {title: "Panier", message: "Ça semble fonctionner!"});
+    res.render("../views/pages/shopping-cart", {title: "Panier", shoppingCartCount: 5 });
 });
 
 router.get("/commande", (req, res) => {
-    res.render("../views/pages/order", {title: "Commande", message: "Ça semble fonctionner!"});
+    res.render("../views/pages/order", {title: "Commande", shoppingCartCount: 5 });
 });
 
 router.get("/confirmation", (req, res) => {
-    res.render("../views/pages/confirmation", {title: "Confirmation", message: "Ça semble fonctionner!"});
+    res.render("../views/pages/confirmation", {title: "Confirmation", shoppingCartCount: 5 });
 });
 
 module.exports = router;
