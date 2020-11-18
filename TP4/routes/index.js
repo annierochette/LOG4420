d34324ead534a2f484ffae082a5b54d97e6264b4
maxime.bernier <maxime.bernier@polymtl.ca>
2020-11-17 19:17:01 -0500

    tp4: Product ejs done

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -4,17 +4,26 @@ const router = express.Router();
 
 const Product = mongoose.model('Product');
 
+function formatPrice(price) {
+    return price.toFixed(2).replace(".", ",") + "&thinsp;$";
+  }
+
 router.get(["/", "/accueil"], (req, res) => {
     res.render("../views/pages/index", {title: "Accueil", shoppingCartCount: 5 });
 });
 
 router.get("/produits", async (req, res) => {
     const products = await Product.find({}).collation({ locale: "en" }).sort({"price": 1});
-    res.render("../views/pages/products", {title: "Produits", shoppingCartCount: 5, productList: products });
+    res.render("../views/pages/products", {title: "Produits", shoppingCartCount: 5, productList: products, formatPrice: formatPrice });
 });
 
-router.get("/produits/:id", (req, res) => {
-    res.render("../views/pages/product", {title: "Produit", shoppingCartCount: 5 });
+router.get("/produits/:id", async (req, res) => {
+    try {
+        const productList = await Product.find({id: req.params.id});
+        res.render("../views/pages/product", {title: "Produit", shoppingCartCount: 5, product: productList[0], formatPrice: formatPrice });
+    } catch (err) {
+        res.sendStatus(500);
+    }
 });
 
 router.get("/contact", (req, res) => {
